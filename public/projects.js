2cf5be10ccf996fb062bee831654f35e2cd8bf54
Timothy Dennis <timothyddennis@gmail.com>
2023-09-29 14:39:21 -0500

    fix: tweaked js to make hidden elements have consistent style with rest of site

--- public/projects.js
+++ public/projects.js
@@ -8,8 +8,8 @@ firstSynopsis.onclick = function() {
     let explanation = firstSynopsis.lastElementChild;
     if (explanation.style.display === "") {
         explanation.style.display = "inline-block";
-        explanation.parentNode.style.backgroundColor = "hsl(150, 50%, 80%)";
-        explanation.parentNode.style.color = "hsl(150, 50%, 20%)";
+        explanation.parentNode.style.backgroundColor = "hsl(230, 50%, 80%)";
+        explanation.parentNode.style.color = "hsl(230, 50%, 20%)";
     } else if (explanation.style.display === "inline-block") {
         explanation.style.display = "";
         explanation.parentNode.style.backgroundColor = "";
@@ -21,8 +21,8 @@ secondSynopsis.onclick = function() {
     let explanation = secondSynopsis.lastElementChild;
     if (explanation.style.display === "") {
         explanation.style.display = "inline-block";
-        explanation.parentNode.style.backgroundColor = "hsl(150, 50%, 80%)";
-        explanation.parentNode.style.color = "hsl(150, 50%, 20%)";
+        explanation.parentNode.style.backgroundColor = "hsl(230, 50%, 80%)";
+        explanation.parentNode.style.color = "hsl(230, 50%, 20%)";
     } else if (explanation.style.display === "inline-block") {
         explanation.style.display = "";
         explanation.parentNode.style.backgroundColor = "";
