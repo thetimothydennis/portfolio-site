7fb439087e20390af9410e1476b9b34115ed0d69
Timothy Dennis <timothyddennis@gmail.com>
2023-09-29 12:06:14 -0500

    fix: adjusted port for HTTP server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 import express from "express";
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.use(express.static("./public/"));
 
 app.listen(port, () => {
     console.log(`app is listening on port ${port}`);
-})
+});
