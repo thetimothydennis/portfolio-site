1061051acbf7396d4296c64f8fba88882fd10098
Timothy Dennis <timothyddennis@gmail.com>
2023-10-03 08:17:02 -0500

    fix: refactored projects.js to be less repetitive

--- public/projects.js
+++ public/projects.js
@@ -1,45 +1,21 @@
 
+function toggleHidden() {
+    let explanation = this.lastElementChild;
+    if (explanation.style.display === "") {
+        explanation.style.display = "inline-block";
+        explanation.parentNode.style.backgroundColor = "hsl(230, 50%, 80%)";
+        explanation.parentNode.style.color = "hsl(230, 50%, 20%)";
+    } else if (explanation.style.display === "inline-block") {
+        explanation.style.display = "";
+        explanation.parentNode.style.backgroundColor = "";
+        explanation.parentNode.style.color = "";
+    }
+}
 
 let firstSynopsis = document.getElementById("project1");
 let secondSynopsis = document.getElementById("project2");
 let thirdSynopsis = document.getElementById("project3");
 
-
-firstSynopsis.onclick = function() {
-    let explanation = firstSynopsis.lastElementChild;
-    if (explanation.style.display === "") {
-        explanation.style.display = "inline-block";
-        explanation.parentNode.style.backgroundColor = "hsl(230, 50%, 80%)";
-        explanation.parentNode.style.color = "hsl(230, 50%, 20%)";
-    } else if (explanation.style.display === "inline-block") {
-        explanation.style.display = "";
-        explanation.parentNode.style.backgroundColor = "";
-        explanation.parentNode.style.color = "";
-    }
-}
-
-secondSynopsis.onclick = function() {
-    let explanation = secondSynopsis.lastElementChild;
-    if (explanation.style.display === "") {
-        explanation.style.display = "inline-block";
-        explanation.parentNode.style.backgroundColor = "hsl(230, 50%, 80%)";
-        explanation.parentNode.style.color = "hsl(230, 50%, 20%)";
-    } else if (explanation.style.display === "inline-block") {
-        explanation.style.display = "";
-        explanation.parentNode.style.backgroundColor = "";
-        explanation.parentNode.style.color = "";
-    }
-}
-
-thirdSynopsis.onclick = function() {
-    let explanation = thirdSynopsis.lastElementChild;
-    if (explanation.style.display === "") {
-        explanation.style.display = "inline-block";
-        explanation.parentNode.style.backgroundColor = "hsl(230, 50%, 80%)";
-        explanation.parentNode.style.color = "hsl(230, 50%, 20%)";
-    } else if (explanation.style.display === "inline-block") {
-        explanation.style.display = "";
-        explanation.parentNode.style.backgroundColor = "";
-        explanation.parentNode.style.color = "";
-    }
-}
+firstSynopsis.addEventListener("click", toggleHidden);
+secondSynopsis.addEventListener("click", toggleHidden);
+thirdSynopsis.addEventListener("click", toggleHidden);
